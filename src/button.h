e0c0e18658a733e0355c44eaaa105233ea1f5c37
syeminpark <70131115+syeminpark@users.noreply.github.com>
2021-02-09 22:14:04 +0900

    tft_esp32

---
+++ src/button.h
@@ -0,0 +1,34 @@
+#ifndef button_h
+#define button_h
+
+void IRAM_ATTR enterToggle()
+{
+    enterState = !enterState;
+}
+
+void IRAM_ATTR exitToggle()
+{
+    exitState = !exitState;
+    W
+}
+
+void enter()
+{
+    if (enterState == LOW)
+    {
+        //
+        enterState = !enterState;
+        Serial.println("enterClicked");
+    }
+}
+
+void exit()
+{
+    if (exitState == LOW)
+    {
+        //
+        exitState = !exitState;
+        Serial.println("exitClicked");
+    }
+}
+#endif
